08a0108be1da633a8b6fe6fe9576ce374daafde3
kevinguevaradevia@gmail.com <kevinguevaradevia@gmail.com>
2026-03-18 16:42:58 -0500

    fix svg icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Greyhaven Docs',
   tagline: 'Sandbox and proxy tools for running semi-trusted commands safely',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   future: {
     v4: true,
